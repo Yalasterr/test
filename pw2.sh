761ddd047dcf512435cf63a43fb89453ca2a2a4c
Yalasterr <yalasterr@gmail.com>
2024-11-22 21:33:26 +0300

    Update pw2.sh

--- pw2.sh
+++ pw2.sh
@@ -15,6 +15,12 @@ fi
 # Добавляем пользователя student
 USERNAME="student"
 
+# Проверка на существование пользователя
+if id "$USERNAME" &>/dev/null; then
+    echo -e "${RED}Пользователь ${USERNAME} уже существует.${NC}"
+    exit 1
+fi
+
 # Запрашиваем пароль у пользователя
 echo -e "${YELLOW}Введите пароль для пользователя ${USERNAME}:${NC}"
 read -s PASSWORD
@@ -22,11 +28,20 @@ read -s PASSWORD
 # Создаем пользователя с указанным паролем
 useradd -m -p "$(openssl passwd -1 "$PASSWORD")" "$USERNAME"
 
-# Переходим в корневой каталог
-cd /
+# Проверка на наличие каталога 931Gruppa
+if [ -d "/931Gruppa" ]; then
+    echo -e "${RED}Каталог 931Gruppa уже существует.${NC}"
+    exit 1
+fi
 
 # Создаём каталог 931Gruppa
 mkdir 931Gruppa
+
+# Проверка на наличие каталога Forever
+if [ -d "/931Gruppa/Forever" ]; then
+    echo -e "${RED}Каталог Forever уже существует.${NC}"
+    exit 1
+fi
 
 # Переходим в каталог 931Gruppa и создаём папку Forever
 cd 931Gruppa
@@ -52,6 +67,13 @@ date -s "09:35:00"
 
 # Определяем объём оперативной памяти компьютера
 free -h
+
+# Проверка на установленный net-tools
+if ! command -v ifconfig &> /dev/null; then
+    echo -e "${YELLOW}Пакет net-tools не установлен. Устанавливаем его...${NC}"
+    apt update && apt install -y net-tools
+
+fi
 
 # Копируем файл good.txt в каталог GR931 
 cp GR931/Forever/good.txt /GR931/
